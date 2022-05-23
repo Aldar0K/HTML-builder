ae370443d652744c7ebe5d3c3bdbb4ced3fafaa7
Aldar <aldarkonov@mail.ru>
2022-05-23 20:12:53 +0300

    fix: change solution for 3 task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -11,11 +11,11 @@ fs.readdir(pathToFolder, (err, files) => {
       if (err) throw err;
 
       if (stats.isFile()) {
-        let fileName = files[i].split('.')[0];
-        let extName = path.extname(files[i]).slice(1);
-        let fileSize = (stats.size / 1024).toFixed(3) + 'kb';
+        const fileExt = path.extname(files[i]);
+        const fileName = path.basename(files[i], fileExt);
+        const fileSize = (stats.size / 1024).toFixed(3) + 'kb';
 
-        console.log(fileName, '-', extName, '-', fileSize);
+        console.log(fileName, '-', fileExt.slice(1), '-', fileSize);
       };
     });
   };
