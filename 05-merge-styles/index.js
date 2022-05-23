f9304f3e2f2e76eac6d83537a4f36d0cb02986ef
Aldar <aldarkonov@mail.ru>
2022-05-23 20:43:49 +0300

    feat: finish all tasks

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -5,7 +5,6 @@ const pathToInputFolder = path.join(__dirname, 'styles');
 const pathToOutputFolder = path.join(__dirname, 'project-dist');
 
 const output = fs.createWriteStream(path.join(pathToOutputFolder, 'bundle.css'));
-
 console.log('bundle.css created successfully!');
 
 fs.readdir(pathToInputFolder, (err, files) => {
@@ -14,13 +13,9 @@ fs.readdir(pathToInputFolder, (err, files) => {
     for (let i = 0; i < files.length; i++) {
         if (path.extname(files[i]).slice(1) === 'css') {
             const input = fs.createReadStream(path.join(pathToInputFolder, files[i]));
-
             let data = '';
-
             input.on('data', chunk => data += chunk);
-
             input.on('error', error => console.log('Error', error.message));
-
             input.on('end', () => output.write(data));
 
             console.log('css file implemented successfully!')
